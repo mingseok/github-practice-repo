1037758e0fb881af7de0cb4282bdd3232f0807a9
Minseok Kim <alstjr706@naver.com>
2024-02-11 22:07:18 +0900

    Update GithubApplication.java

--- src/main/java/com/example/github/GithubApplication.java
+++ src/main/java/com/example/github/GithubApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GithubApplication {
 
-	// git Test 24.02.10
+	// git Test 24.02.11
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubApplication.class, args);
